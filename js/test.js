6638c62aba152563d73dba0aaff586ff2a6a7bb3
Woohyun Kang <30944771+KangWH@users.noreply.github.com>
2025-11-22 16:39:58 +0900

    Unnecessary files removed

--- js/test.js
+++ js/test.js
@@ -1,12 +1,6 @@
 (async () => {
   await activeInputSources.addSource('qwertyUS');
   activeInputSources.useSource(0);
-  // await activeInputSources.addSource('dubeolsik');
-  // await activeInputSources.addSource('dubeolsikNorth');
   await activeInputSources.addSource('dubeolsikYethangul');
-  // await activeInputSources.addSource('sebeolsik390');
   await activeInputSources.addSource('sebeolsik391');
-  // await activeInputSources.addSource('sebeolsikNoshift');
-  // await activeInputSources.addSource('sebeolsikYethangul');
-  await activeInputSources.addSource('romaja');
 })();
